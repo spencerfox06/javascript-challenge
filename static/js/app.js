07e663bfc71e94ece51c98b654fecc3a0b754961
spencer fox <spencerfox@outlook.com>
2021-06-14 14:08:07 -0400

    filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form --- NOT SURE ABOUT THIS
+var form = d3.select("#filters");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter(event) {
+
+    // Prevent the page from refreshing
+    // event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select('#datetime')
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property('value')
+    console.log(inputValue)
+
+    // Use the form input to filter the data by blood type
+    var filteredData = tableData.filter(dateobs => dateobs.datetime === inputValue);
+    console.log(filteredData)
+}
