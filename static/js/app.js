cca7a6081e69dcf2b99e534ab42cb09c96fd0316
spencer fox <spencerfox@outlook.com>
2021-06-15 15:32:06 -0400

    final

--- static/js/app.js
+++ static/js/app.js
@@ -13,29 +13,33 @@ button.on("click", runEnter);
 form.on("submit", runEnter);
 
 // Complete the event handler function for the form
-function runEnter(event) {
+function runEnter() {
 
-    // Prevent the page from refreshing
-    // event.preventDefault();
+  // Prevent the page from refreshing
+  // event.preventDefault();
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select('#datetime')
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select('#datetime')
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property('value')
-    console.log(inputValue)
+  // Get the value property of the input element
+  var inputValue = inputElement.property('value')
+  console.log(inputValue)
+  var filteredData = data
 
-    // Use the form input to filter the data by blood type
-    var filteredData = tableData.filter(dateobs => dateobs.datetime === inputValue);
+  // Use the form input to filter the data by blood type
+  if (inputValue) {
+    filteredData = tableData.filter(dateobs => dateobs.datetime === inputValue);
     console.log(filteredData)
-
-    var tempbody = d3.select('tbody');
-        tempbody.html("")
-        data.forEach(filteredData => {
-        var temprow = tempbody.append('tr')
-        Object.entries(filteredData).forEach(([ key, value ]) => {
-            var tabledata = temprow.append('td')
-            tabledata.text(value)
+  }
+  var tempbody = d3.select('tbody');
+  tempbody.html("")
+  filteredData.forEach(row => {
+    var temprow = tempbody.append('tr')
+    Object.entries(row).forEach(([key, value]) => {
+      var tabledata = temprow.append('td')
+      tabledata.text(value)
     })
   })
 }
+
+runEnter();
